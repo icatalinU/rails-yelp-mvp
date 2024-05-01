d6264018ce5099a98599d3fcc7d8a862e71a7578
catalin ursu <ursucatalin0@gmail.com>
2024-05-01 16:19:52 +0100

    restaurant review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,12 @@
+ 
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -14,15 +16,34 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to @restaurant
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
     else
       render :new
     end
   end
 
-  private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  def edit
   end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  private
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :category)
+    end
 end
